7989974d432bb333f4d24fa58a0f4eceb924a104
Ricki <rickixie@users.noreply.github.com>
2023-01-10 15:11:58 -0800

    update api response to test

--- api/message/index.js
+++ api/message/index.js
@@ -14,6 +14,6 @@
 
 module.exports = async function (context, req) {
     context.res.json({
-        text: "Hello from the API"
+        text: "Hello from the API - new update 2022/1/10"
     });
 };
